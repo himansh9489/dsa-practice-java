637c5de47f8016db4dd00922e8eb28b5004aa48a
Himanshu Kushwah <himansh9489@gmail.com>
2024-02-01 20:18:59 +0530

    dsa practice in java

---
+++ Demo.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Demo
+ */
+public class Demo {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int n = sc.nextInt();
+        // Long l = sc.nextLong();
+        // Float f = sc.nextFloat();
+        // Double d = sc.nextDouble();
+        // boolean bool = sc.nextBoolean();
+        // Byte b = sc.nextByte();
+        // BigInteger bi = sc.nextBigInteger();
+
+        System.out.println(str);
+        System.out.println(n);
+        // System.out.println(l);
+        // System.out.println(f);
+        // System.out.println(d);
+        // System.out.println(bool);
+        // System.out.println(b);
+        sc.close();
+
+    }
+}
